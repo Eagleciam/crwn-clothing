9f31db6670b19ab31fbf3bcc70966269d5c2206d
Cesar Alarcon <ciam042@gmail.com>
2020-04-21 17:15:37 -0500

    finishing sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,19 +17,29 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const {email, password} = this.state;
+
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    }catch(error){
+      console.log(error)
+
+    } 
   };
 
-  handleChange = event => {
+  handleChange = async event => {
     const { value, name } = event.target;
 
-    this.setState({ [name]: value });
-  };
 
-  render() {
+
+    this.setState({ [name]: value });
+  }
+
+  render(){
     return (
       <div className='sign-in'>
         <h2>I already have an account</h2>
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -50,7 +50,7 @@ class SignUp extends React.Component{
     handleChange = event => {
         const {name, value} = event.target;
 
-        this.setState({ [name]: value  }, ()=> console.log(this.state))
+        this.setState({ [name]: value  })
 
     }
 
